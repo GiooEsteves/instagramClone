77afccd8d5bcbbde7b52d34a978e5cf8e854ec7a
GiooEsteves <100709042+GiooEsteves@users.noreply.github.com>
2024-04-13 13:35:33 -0300

    Atualização da authpage: - Box de instalação - Box de Entrar e cadastra-se

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,16 +6,22 @@ const AuthPage = () =>{
   return(
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}> 
         <Container maxW={"container.md"} padding={0}>
-            {/* LADO ESQUERDO */}
-            <Box display={{base:"none", md:"block"}}>
-                <Image src="/auth.png" h={650} alt='Phone img'/>
-            </Box>
-            {/* LADO DIREITO */}
-            <VStack spacing={4} align={"stretch"}>
-                <AuthForm>
-                    
-                </AuthForm>
-            </VStack>
+            <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+                {/* LADO ESQUERDO */}
+                <Box display={{base:"none", md:"block"}}>
+                    <Image src="/auth.png" h={650} alt='Phone img'/>
+                </Box>
+
+                {/* LADO DIREITO */}
+                <VStack spacing={4} align={"stretch"}>
+                    <AuthForm />
+                    <Box textAlign={"center"}>Obtenha o aplicativo.</Box>
+                    <Flex gap={5} justifyContent={"center"}>
+                        <Image src='/playstore.png' h={"10"} alt='Playstore logo'/>
+                        <Image src='/microsoft.png' h={"10"} alt='Microsoft logo'/>
+                    </Flex>
+                </VStack>
+            </Flex>
         </Container>    
     </Flex>
   )
